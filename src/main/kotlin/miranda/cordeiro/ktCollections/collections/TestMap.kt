4e6d07e2c89d00e3dc80a55704efa186dd91a458
Ewerton Cordeiro Miranda <sonardevplus@gmail.com>
2021-03-03 14:15:20 -0300

    Aula 07 - Map

---
+++ src/main/kotlin/miranda/cordeiro/ktCollections/collections/TestMap.kt
@@ -0,0 +1,17 @@
+package miranda.cordeiro.ktCollections.collections
+
+fun main(){
+    val pair:Pair<String,Double> = Pair("Tomas",1000.00)
+
+    val map1 = mapOf(pair)
+
+    map1.forEach{(k,v) -> println("Chave: $k ,Valor: $v")}
+    
+    val map2 = mapOf(
+        "Pedro" to 2.500 ,
+        "Maria" to 3.000,
+        "Roberto" to 5.500)
+    println("******************************************************")
+    map2.forEach{(k,v) -> println("Chave : $k , Valor : $v")}
+
+}
